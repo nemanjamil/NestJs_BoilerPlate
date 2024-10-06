febe9e8b4418db50d2c941e36ef94af2fed67886
Nemanja Milivojevic <nemanjamil@gmail.com>
2024-10-06 19:25:45 +0200

    Add DB init

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,34 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+  Length,
+  Matches,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  public firstname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  public lastname: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  public email: string;
+
+  @IsString()
+  // @Length(8, 20)
+  // @Matches(/(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])/)
+  @IsNotEmpty()
+  public password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  public username: string;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,41 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+@Index(['email'], { unique: true })
+@Index(['username'], { unique: true })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  firstname: string;
+
+  @Column('text')
+  lastname: string;
+
+  @Column({ length: 255 })
+  email: string;
+
+  @Column('text')
+  password: string;
+
+  @Column({ length: 255 })
+  username: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  //   @OneToMany(() => BadgeAssertion, (badgeAssertion) => badgeAssertion.user)
+  //   badgeAssertions: BadgeAssertion[];
+}
